14919c6d3054a6eb0567e1622d346269f86d1952
Maddie Kirwin <kirwinma@grinnell.edu>
2019-02-04 08:58:42 -0600

    ptrace skeleton code

---
+++ sandboxing/ptrace.c
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <asm/unistd.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/user.h>
